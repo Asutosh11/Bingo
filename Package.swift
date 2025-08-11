14b43f42daafeacde1ce50a378599a98b664ef26
Asutosh Panda <panda.asutosh04@gmail.com>
2025-08-11 20:17:52 +0530

    Fix Package.swift to reference v1.0.4 with correct checksum

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BingoSDK",
-            url: "https://github.com/Asutosh11/Bingo/releases/download/v1.0.3/BingoSDK.xcframework.zip",
-            checksum: "your-checksum-here"
+            url: "https://github.com/Asutosh11/Bingo/releases/download/v1.0.4/BingoSDK.xcframework.zip",
+            checksum: "22dab4b7887e2604a3910fb5bf330f3c36a7e09e0cd0315b4e384f6884f61056"
         )
     ]
 )
